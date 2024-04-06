965d37cc41d3d4dff2c0a27ac04e900e1e3c5250
Marco Marchionni <marco.marchionni@gmail.com>
2024-04-07 01:22:47 +0200

    Added debouncing mechanism to search field

--- client/phonebook-app/src/components/ContactsFilter.jsx
+++ client/phonebook-app/src/components/ContactsFilter.jsx
@@ -1,16 +1,23 @@
+import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 
 const ContactsFilter = ({ onSearchTermChange }) => {
-  const handleSearchTermChange = (e) => {
-    console.log(`Search term changed - ${e.target.value}`);
-    onSearchTermChange(e.target.value);
-  };
+  const [typedValue, setTypedValue] = useState("");
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      console.log(`Search term changed - ${typedValue}`);
+      onSearchTermChange(typedValue);
+    }, 500); // Waits 500ms after the user stops typing to make the API call
+
+    return () => clearTimeout(timerId); // Cleanup the timeout on component unmount or if the value changes
+  }, [typedValue, onSearchTermChange]);
+
   return (
     <Form.Control
-      name="firstName"
       type="text"
       placeholder="Search contacts"
-      onChange={handleSearchTermChange}
+      onChange={(e) => setTypedValue(e.target.value)}
     />
   );
 };
