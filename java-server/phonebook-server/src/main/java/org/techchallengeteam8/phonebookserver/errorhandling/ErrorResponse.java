263839ff57362df147cddc63233a0da8db7daf09
Marco Marchionni <marco.marchionni@gmail.com>
2024-04-08 17:39:36 +0200

    Basic error handling

---
+++ java-server/phonebook-server/src/main/java/org/techchallengeteam8/phonebookserver/errorhandling/ErrorResponse.java
@@ -0,0 +1,15 @@
+package org.techchallengeteam8.phonebookserver.errorhandling;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+}
